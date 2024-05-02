34e00acaa6db2040db035d2d964df61625c5e947
uhtetaungkyaw <htetaungkyaw372005@gmail.com>
2024-05-02 18:38:11 +0630

    Initial commit

---
+++ src/test/java/org/solt/facebooklogin/FacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.solt.facebooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
